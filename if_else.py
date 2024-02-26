0f02aaa3ce9cc8d84e091738211e3957df3099cb
iasaduzzaman <zasad.cse@gmail.com>
2024-02-26 23:55:26 +0600

    add oneline if else statement

--- if_else.py
+++ if_else.py
@@ -1,8 +1,10 @@
 a=220
-b=10
-if a>b:
+b=30
+"""if a>b:
     print("A is greater than B")
 elif a == b:
     print("A is equal to B")
 else:
-    print("B ia greater than A")
+    print("B ia greater than A") """
+print("a") if a>b else print("b")
+
