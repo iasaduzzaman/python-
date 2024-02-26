3dfc000d9b87b897a9e7dd91fc6f5d6e467c2daf
iasaduzzaman <zasad.cse@gmail.com>
2024-02-27 00:03:11 +0600

    add and operator

--- if_else.py
+++ if_else.py
@@ -1,5 +1,6 @@
 a=220
 b=30
+c=500
 """if a>b:
     print("A is greater than B")
 elif a == b:
@@ -7,4 +8,6 @@ elif a == b:
 else:
     print("B ia greater than A") """
  #print("a") if a>b else print("b")  #in onloine if else condition
-print("a") if a>b else print("=") if a == b else print("b")
+ #print("a") if a>b else print("=") if a == b else print("b")
+if a > b and c > a:
+    print("Both condition are true")
