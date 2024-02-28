e74424539b01883f2a80c4d5450270249bcf1fb7
iasaduzzaman <zasad.cse@gmail.com>
2024-02-28 23:54:24 +0600

    add nested if

--- if_else.py
+++ if_else.py
@@ -1,5 +1,5 @@
-a=220
-b=30
+#a=220
+#b=30
 #c=500
 """if a>b:
     print("A is greater than B")
@@ -12,5 +12,14 @@ else:
 #if a > b and c > a:
  #if a > b or c > a:
  #print("Atleast one of the condition is true")
-if not b>a:
-    print("b is not greater than a")
+#if not b>a:
+   # print("b is not greater than a")
+    #nested if
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
