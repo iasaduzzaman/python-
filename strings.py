94a46c340091ad7c4e1cfb65a3c7857922b51166
iasaduzzaman <zasad.cse@gmail.com>
2024-02-29 23:59:16 +0600

    add first slicing

--- strings.py
+++ strings.py
@@ -1,3 +1,8 @@
 print("hello")
 b = "Hello, World!"   # slicing string
 print(b[2:5])
+b = "Hello, World!"
+print(b[:5])            # slicing from start to position 5
+
+b = "Hello, World!"      # slicing from position 2 and all the way to end
+print(b[2:])
