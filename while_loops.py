093af367e8fb8e1bb0915e8e74f74cb1deddbb5b
iasaduzzaman <zasad.cse@gmail.com>
2024-02-29 23:44:58 +0600

    add while loop using break

--- while_loops.py
+++ while_loops.py
@@ -3,7 +3,7 @@ while i < 6:
   print(i)
   i += 1"""
 i = 1
-while i < 6:
+while i < 6:     # With the break statement we can stop the loop even if the while condition is true
   print(i)
   if i == 3:
     break
