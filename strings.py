3afa3398513c8c48cb5d72bf7702fadda5285c12
iasaduzzaman <zasad.cse@gmail.com>
2024-03-01 00:01:40 +0600

    add upper method

--- strings.py
+++ strings.py
@@ -6,3 +6,6 @@ print(b[:5])            # slicing from start to position 5
 
 b = "Hello, World!"      # slicing from position 2 and all the way to end
 print(b[2:])
+
+a = "Hello, World!"
+print(a.upper())         # upper method return the string
