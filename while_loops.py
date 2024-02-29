583b3d6cb2d0ccdd9f6ea6219d3100a35666b245
iasaduzzaman <zasad.cse@gmail.com>
2024-02-29 23:46:52 +0600

    add continue in while loop

--- while_loops.py
+++ while_loops.py
@@ -2,9 +2,16 @@
 while i < 6:
   print(i)
   i += 1"""
-i = 1
+"""i = 1
 while i < 6:     # With the break statement we can stop the loop even if the while condition is true
   print(i)
   if i == 3:
     break
+  i += 1"""
+# With the continue statement we can stop the current iteration, and continue with the next
+i = 0
+while i < 6:
   i += 1
+  if i == 3:
+    continue
+  print(i)
