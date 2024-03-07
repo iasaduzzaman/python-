983bbc1aa711c77145652c5d061413f659f58573
iasaduzzaman <zasad.cse@gmail.com>
2024-03-07 09:19:34 +0600

    add new for loop print 1 to 100

--- for_loops.py
+++ for_loops.py
@@ -3,8 +3,10 @@ for x in fruits:
   print(x)"""
 """for x in "banana":
   print(x) """
-fruits = ["apple", "banana", "cherry"]
+"""fruits = ["apple", "banana", "cherry"]
 for x in fruits:
   if x == "banana":
     break
-  print(x)
+  print(x) """
+for i in range(1,101):
+    print(i,end=" ")
